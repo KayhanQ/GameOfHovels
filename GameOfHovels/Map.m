cfa289062627924d1034e0ee195865c6c22be2b7
KayhanQ <Kayhan@Kahan-Feroze-Qaisers-iMac.local>
2015-03-30 10:04:52 -0400

    Revert "Revert 7e51b52..a566733"
    This reverts commit 8cd3e1805b832e2793cc30e407995aa4ef2026ab.

--- GameOfHovels/Map.m
+++ GameOfHovels/Map.m
@@ -745,9 +745,10 @@
 - (void)beginTurnPhases
 {
     [self treeGrowthPhase];
+    [self tombstonePhase];
+    [self incomePhase];
+    [self paymentPhase];
     [self buildPhase];
-    
-    
 }
 
 - (void)treeGrowthPhase
@@ -770,18 +771,16 @@
     }
 }
 
-- (void)tombstonePhase //Any tombstones on tiles owned by the player are replaced by trees
+//Any tombstones on tiles owned by the player are replaced by trees
+- (void)tombstonePhase
 {
     for (Tile* vTile in [self getTilesWithMyVillages]) {
         for (Tile* t in [self getTilesforVillage:vTile.village]) {
             if ([t hasTombstone]) {
-                
                 [t removeStructure];
                 [t addStructure:BAUM];
-                
             }
         }
-    
     }
 }
 
@@ -791,75 +790,53 @@
 {
     for (Tile* vTile in [self getTilesWithMyVillages]) {
         for (Tile* t in [self getTilesforVillage:vTile.village]) {
-            
             switch([t getStructureType]){
-                    
                 case MEADOW:
+                {
                     vTile.village.goldPile += 2;
                     break;
-                    
-                case NONE:
+                }
+                case GRASS:
+                {
                     vTile.village.goldPile += 1;
-                    
                     break;
-                    
+                }
                     default:
+                {
                     break;
-      
+                }
                     
             }
-            
-            
         }
     }
-
 }
 
 //also known as upkeep phase
-- (void)paymentPhase // Money is subtracted from each village’s treasury based on the villagers that it supports. If a village has insufficient funds to pay the villagers it supports, all villagers supported by that village perish and are replaced by tombstones.
+// Money is subtracted from each village’s treasury based on the villagers that it supports. If a village has insufficient funds to pay the villagers it supports, all villagers supported by that village perish and are replaced by tombstones.
+- (void)paymentPhase
 {
-    
     for (Tile* vTile in [self getTilesWithMyVillages]) {
-        
-        if(vTile.village.goldPile < 0){
-            
-            //[vTile killAllVillagers];
+        if(vTile.village.goldPile < 0) {
+            [self killAllVillagers: vTile];
             break;
-            
         }
         
         for (Tile* t in [self getTilesforVillage:vTile.village]) {
-            
-            if([t hasUnit]){
-                
+            if([t hasUnit]) {
                 vTile.village.goldPile -= t.unit.upkeepCost;
-                
-              
-                
             }
-            
-            
-            
         }
     }
-    
-    
 }
 
--(void)killAllVillagers:(Tile*)village;
+-(void)killAllVillagers:(Tile*)villageTile;
 {
- 
-        for (Tile* t in [self getTilesforVillage:village.village]){
-        
-            if([t hasUnit]){
-                [t removeUnit];
-                [t addStructure:TOMBSTONE];
-                
-            }
-            
+    for (Tile* t in [self getTilesforVillage:villageTile.village]){
+        if([t hasUnit]){
+            [t removeUnit];
+            [t addStructure:TOMBSTONE];
         }
-            
-    
+    }
 }
 
 - (void)buildPhase
