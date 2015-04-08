490c1ac1f873341d49417a413c084aa6674e9643
KayhanQ <kayhanfqaiser@gmail.com>
2015-04-08 18:19:24 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit c448af91307a7aa9aa1539027a2910d058e6e97f, reversing
    changes made to e4ecda3f0921fb9bbb5ad747dfed074d935abc7a.

--- GameOfHovels/Tile.h
+++ GameOfHovels/Tile.h
@@ -45,8 +45,6 @@
 - (void)mergeVillageBySwallowing:(Village*)v;
 - (BOOL)hasVillage;
 - (BOOL)isVillage;
-- (enum VillageType)getVillageType;
-- (Village*)getVillage;
 
 
 - (void)addUnitWithType:(enum UnitType)uType;
